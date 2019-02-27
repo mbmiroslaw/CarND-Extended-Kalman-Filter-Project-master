53a475a4001c4dbdaaa43d7b641a44ba2956122b
mbmiroslaw <37596349+mbmiroslaw@users.noreply.github.com>
2019-02-27 18:02:46 +0100

    Add files via upload

---
+++ kalman_filter.cpp
@@ -0,0 +1,92 @@
+#include "kalman_filter.h"
+#include "tools.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+  /**
+  TODO:
+    * predict the state
+  */
+	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Kalman Filter equations
+  */
+	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd S = H_ * PHt + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Extended Kalman Filter equations
+  */
+	float rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
+	float phi = atan2(x_(1), x_(0));
+	const double pi = 3.1415926535897;
+
+		
+	float rhod;
+	
+	
+	if (fabs(rho) < 0.0001) {
+		rhod = 0;
+	} else {
+		rhod = (x_(0)*x_(2) + x_(1)*x_(3))/rho;
+	}
+	
+	VectorXd z_pred(3);
+	z_pred << rho, phi, rhod;
+	VectorXd y = z - z_pred;
+		
+	while(y(1) > pi){
+		y(1) -=2*pi;
+	}
+	
+	while(y(1) < -pi){
+		y(1) +=2*pi;
+	}
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
+}
